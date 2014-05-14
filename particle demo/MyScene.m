68a8d1ffbaf6b0c4076de848aec5165039d81735
bradleypj@icloud.com <Brad@Bradleys-MacBook-Pro.local>
2014-05-14 09:06:19 -0700

    first push

--- particle demo/MyScene.m
+++ particle demo/MyScene.m
@@ -5,25 +5,75 @@
 //  Created by Bradley Johnson on 5/13/14.
 //  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
 //
-
+@import CoreMotion;
 #import "MyScene.h"
+
+@interface MyScene ()
+
+@property (strong,nonatomic) CMMotionManager *motionManager;
+
+@end
 
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
+        
+        
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:0 green:0 blue:0 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+          SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        sprite.position = CGPointMake(160, 100);
+        sprite.size = CGSizeMake(100, 100);
         
-        [self addChild:myLabel];
+        
+        NSString *starsPath =
+        [[NSBundle mainBundle] pathForResource:@"Stars" ofType:@"sks"];
+        
+        SKEmitterNode *stars = [NSKeyedUnarchiver unarchiveObjectWithFile:starsPath];
+        
+        stars.position = CGPointMake(160, 560);
+        
+        [self addChild:stars];
+        [self addChild:sprite];
+        
+        NSString *exhaustPath =
+        [[NSBundle mainBundle] pathForResource:@"Exhaust" ofType:@"sks"];
+        
+        SKEmitterNode *exhaust = [NSKeyedUnarchiver unarchiveObjectWithFile:exhaustPath];
+        exhaust.position = CGPointMake(0, -50);
+        
+        [sprite addChild:exhaust];
+        
+        
+        self.motionManager = [[CMMotionManager alloc] init];
+        
+        self.motionManager = [[CMMotionManager alloc] init];
+        if ([self.motionManager isAccelerometerAvailable])
+        {
+            [self.motionManager setAccelerometerUpdateInterval:1/30];
+            
+            [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue new] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+                
+                NSLog(@"%f",accelerometerData.acceleration.x);
+                
+                sprite.position = CGPointMake(sprite.position.x + (accelerometerData.acceleration.x * 3), sprite.position.y);
+                
+                //self.physicsWorld.gravity =  CGVectorMake(accelerometerData.acceleration.x * 5, -5);
+                
+                
+            }];
+        }
+
+        
+        
+        
+       
+        
+
     }
     return self;
 }
